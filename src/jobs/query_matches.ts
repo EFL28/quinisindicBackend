481e88bf4f74aee127b81f83fff5cd7e182773d2
Ubuntu <nfolquer@gmail.com>
2025-04-26 17:29:19 +0000

    Job function

--- src/jobs/query_matches.ts
+++ src/jobs/query_matches.ts
@@ -1,4 +1,10 @@
 import { LaLigaService } from '../modules/la_liga/laliga.service';
-
+import { Redis } from "ioredis"
+const redis = new Redis();
 const service = new LaLigaService();
-console.log(await service.getEventsCron());
+const liga_data = await service.getEventsCron();
+liga_data['last_fecthed'] = Date.now()
+console.log(liga_data)
+await redis.set("la_liga", JSON.stringify(liga_data))
+console.log("Data fetched")
+await redis.quit();
